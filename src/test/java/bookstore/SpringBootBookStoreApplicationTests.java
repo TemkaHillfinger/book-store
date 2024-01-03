2790342afa07f8d93bc8af96897f39f0230e6819
Artem <artempharm83@gmail.com>
2024-01-03 20:57:38 +0200

    first commit

--- src/test/java/bookstore/SpringBootBookStoreApplicationTests.java
+++ src/test/java/bookstore/SpringBootBookStoreApplicationTests.java
@@ -3,7 +3,7 @@ package bookstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootBookStoreApplicationTests.class)
 class SpringBootBookStoreApplicationTests {
 
     @Test
