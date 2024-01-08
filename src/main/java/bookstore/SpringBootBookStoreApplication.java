be8f28f4b51ab567122fd8173c64d07b74c86521
Artem <artempharm83@gmail.com>
2024-01-08 15:12:58 +0200

    added BookDto.class and CreateBookRequestDto.class added Mapstruct and mappers Return and accept DTOs on the service layer added missing methods in the BookService and BookRepository added and use EntityNotFoundException class

--- src/main/java/bookstore/SpringBootBookStoreApplication.java
+++ src/main/java/bookstore/SpringBootBookStoreApplication.java
@@ -1,13 +1,9 @@
 package bookstore;
 
-import bookstore.model.Book;
 import bookstore.service.BookService;
-import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootBookStoreApplication {
@@ -16,18 +12,6 @@ public class SpringBootBookStoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBookStoreApplication.class, args);
-    }
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book theLordOfTheRings = new Book();
-            theLordOfTheRings.setTitle("The Lord of the Rings ");
-            theLordOfTheRings.setAuthor("John Ronald Reuel Tolkien");
-            theLordOfTheRings.setIsbn("978-88-452-9261-3");
-            theLordOfTheRings.setPrice(BigDecimal.valueOf(600));
-            bookService.save(theLordOfTheRings);
-            System.out.println(bookService.findAll());
-        };
     }
 }
--- src/main/java/bookstore/repository/BookRepository.java
+++ src/main/java/bookstore/repository/BookRepository.java
@@ -2,9 +2,12 @@ package bookstore.repository;
 
 import bookstore.model.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
-    Book save(Book book);
+    Book createBook(Book book);
 
     List<Book> findAll();
+
+    Optional<Book> findById(Long id);
 }
