a9edecf372f1ee5cd23e0bb141650fa1f2be3a5b
Artem <artempharm83@gmail.com>
2024-02-12 13:50:44 +0200

    added @column annotation with the required parameter in Book added empty line to the end of file db.changelog-master.yaml

--- src/main/java/bookstore/model/Book.java
+++ src/main/java/bookstore/model/Book.java
@@ -16,21 +16,29 @@ import org.hibernate.annotations.Where;
 @SQLDelete(sql = "UPDATE books SET is_delete = true WHERE id = ?")
 @Where(clause = "is_deleted=false")
 @Table(name = "books")
-
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+
+    @Column(name = "title", nullable = false)
     private String title;
-    @Column(nullable = false)
+
+    @Column(name = "author", nullable = false)
     private String author;
-    @Column(nullable = false, unique = true)
+
+    @Column(name = "isbn", nullable = false, unique = true)
     private String isbn;
-    @Column(nullable = false)
+
+    @Column(name = "price",nullable = false)
     private BigDecimal price;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "coverImage")
     private String coverImage;
-    @Column(nullable = false)
+
+    @Column(name = "isDeleted", nullable = false)
     private boolean isDeleted = false;
 }
