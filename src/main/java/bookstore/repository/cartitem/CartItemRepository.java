ac12a5339fac10db2d93d56308237e09aaff3b4f
Artem <artempharm83@gmail.com>
2024-05-21 17:02:57 +0300

    deleted the annotation @Repository

--- src/main/java/bookstore/repository/cartitem/CartItemRepository.java
+++ src/main/java/bookstore/repository/cartitem/CartItemRepository.java
@@ -4,9 +4,7 @@ import bookstore.model.Book;
 import bookstore.model.CartItem;
 import bookstore.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByShoppingCartAndBook(ShoppingCart shoppingCart, Book book);
 }
